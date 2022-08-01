db8fd58001e64827f054688180e509ce17e2cf71
SuperheroJT <ademolathompson5@gmail.com>
2022-08-01 12:02:25 +0100

    project setup complete
    hephzi06 should create a new branch and start working

---
+++ static/js/app.js
@@ -0,0 +1,27 @@
+var app = angular.module('myApp', ['ngRoute']);
+
+//CONTROLLER FOR TABLE
+app.controller('tableController', [ '$scope', '$http', function ($scope, $http) {
+    $http.get('assets/data/hygieia.json')
+    .then(function(response){
+        console.log(response.data.table);
+        $scope.info = response.data.table;
+    })
+    .catch(function(err){
+        console.log(err)
+    })
+}])
+
+//ROUTING
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+    .when('/home',{
+        templateUrl: "views/main.html#home-top",
+    })
+    .when('/doctors',{
+        templateUrl: "views/doctors.html#doctors-top",
+    })
+    .otherwise({
+        redirectTo: '/home'
+    })
+}])
