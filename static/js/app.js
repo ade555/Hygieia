7a63225ac7749a8f49df9e3debdd5f237849f089
hephzi06 <hephzisoft@gmail.com>
2022-08-03 13:15:11 +0100

    almost done with the filtering just remain the all button
    oga don't forget to push soon oo

--- static/js/app.js
+++ static/js/app.js
@@ -1,36 +1,54 @@
-var app = angular.module('myApp', ['ngRoute']);
+var app = angular.module("myApp", ["ngRoute"]);
 
 //CONTROLLER FOR TABLE
-app.controller('DocCtrl', [ '$scope', '$http', function ($scope, $http) {
-    $http.get('static/data/hygieia.json')
-    .then(function(response){
+app.controller("DocCtrl", [
+  "$scope",
+  "$http",
+  function ($scope, $http) {
+    $http
+      .get("static/data/hygieia.json")
+      .then(function (response) {
         // console.log(response.data.doctors);
         $scope.info = response.data.doctors;
-        var docs =$scope.info;
-        function generalDocs(docs) {
-            
-        }
+        var docs = $scope.info;
+        function generalDocs(docs) {}
         generalDocs(docs);
-    })
-    .catch(function(err){
-        console.log(err)
-    })
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
 
     // FILTER FUNCTIONS
-}])
+    $scope.general = function (x) {
+      $scope.filter = x = "General Doctor";
+    };
+    $scope.optha = function (x) {
+        $scope.filter = x = "Opthalmologist";
+      };
+      $scope.pedri = function (x) {
+        $scope.filter = x = "Pediatrician";
+      };
+      $scope.thera = function (x) {
+        $scope.filter = x = "Theraphist";
+      };
+  },
+]);
 
 //ROUTING
-app.config(['$routeProvider', function ($routeProvider) {
+app.config([
+  "$routeProvider",
+  function ($routeProvider) {
     $routeProvider
-    .when('/home',{
+      .when("/home", {
         templateUrl: "views/main.html#home-top",
-    })
-    .when('/doctors',{
+      })
+      .when("/doctors", {
         templateUrl: "views/doctors.html#doctors-top",
-    })
-    .otherwise({
-        redirectTo: '/home'
-    })
+      })
+      .otherwise({
+        redirectTo: "/home",
+      });
     // $routeProvider.html5Mode(true);
     // $routeProvider.hashPrefix('');
-}])
+  },
+]);
